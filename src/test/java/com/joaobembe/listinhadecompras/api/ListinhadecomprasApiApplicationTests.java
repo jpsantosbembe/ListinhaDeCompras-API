fd3d862ff7a3d81a3708702ad941cb00f17e8665
João Bembe <joaobembe@icloud.com>
2024-04-02 15:42:48 -0300

    Hello World

---
+++ src/test/java/com/joaobembe/listinhadecompras/api/ListinhadecomprasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaobembe.listinhadecompras.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListinhadecomprasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
